e22da8eeac7e383c273a6d03891b7e74eea3dd64
Oryx87 <baudin.mylan@laposte.net>
2025-09-15 17:12:24 +0200

    add: Ex4

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //Ex 1
-console.log("Ex 1 :")
+console.log("Ex 1 :");
 let celsius = 20;
 let fahrenheit = celsius * 9/5 + 32;
 console.log(celsius);
 console.log(fahrenheit);
 
 //Ex 2
-console.log("Ex 2 :")
+console.log("Ex 2 :");
 let largeur = 5;
 let longueur = 10;
 let aire = longueur * largeur;
 console.log(aire);
 
 //Ex 3
-console.log("Ex 3 :")
+console.log("Ex 3 :");
 let nom = "Baudin--Marie";
 let prenom = "Mylan";
 let chaine = prenom + " " + nom;
 console.log(chaine);
+
+//Ex 4
+console.log("Ex 4 :");
+let HT = 100;
+const TVA = 1.2;
+let TTC = HT * TVA;
+console.log(TTC);
