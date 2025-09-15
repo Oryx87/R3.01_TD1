dfa191a1f4aa649f405632bf637ca3df5708a2e0
Oryx87 <baudin.mylan@laposte.net>
2025-09-15 17:05:21 +0200

    add: Ex 1 + 2

---
+++ script.js
@@ -0,0 +1,13 @@
+//Ex 1
+console.log("Ex 1 :")
+let celsius = 20;
+let fahrenheit = celsius * 9/5 + 32;
+console.log(celsius);
+console.log(fahrenheit);
+
+//Ex 2
+console.log("Ex 2 :")
+let largeur = 5;
+let longueur = 10;
+let aire = longueur * largeur;
+console.log(aire);
